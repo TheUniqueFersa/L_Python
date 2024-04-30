b3557d0bea9f7f8d88ffb86e90dbbfab9c14449d
TheUniqueFersa <holafersa@gmail.com>
2024-04-29 20:51:18 -0600

    feat(sesión 4): ternario

---
+++ playgrounds/playground_2.py
@@ -0,0 +1,6 @@
+# Estructuras básicas (if-elif-else, for, while) vistas en otras sesiones
+# TERNARIO:
+edad = int(input("Ingresa tu edad: "))
+mensaje = "Eres mayor de edad" if edad > 18 else "Eres un bebé"
+print(mensaje)
+
