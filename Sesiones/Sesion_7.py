a57ee217c909f9732e405766a26f2e25c3dee708
TheUniqueFersa <holafersa@gmail.com>
2024-05-02 22:46:57 -0600

    feat(sesión 7): añade apuntes, indexing para strings

---
+++ Sesiones/Sesion_7.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------
+# Apuntes - ejemplos
+# --------
+# INDEXING
+texto = "A ella le gusta lenguaje C, es decir, ella es perfecta"
+print(texto[0]) # Como en C :ooo
+print(texto[1]) # Es espacio
+
+#print(texto[999]) # IndexError: string index out of range
+
+print(texto[-1]) # Esto si es válido, lo que hace es ir de regreso
+print(texto[len(texto)-1]) #Línea que hace lo mismo que la de arriba 
+'''(Nótese que dentro del índice) se resta 1, porque len() regresa el tamaño, y, así como los arreglos 
+el índice máximo para cualquier arreglo es el numero n (tamaño) - 1
+'''
+
+
+# ----------------------------------------------------------------------
+# Ejercicios
