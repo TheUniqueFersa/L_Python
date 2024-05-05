ebbf7441b7e004ae80bf9cca54c72610a321a1f5
TheUniqueFersa <holafersa@gmail.com>
2024-05-04 19:54:17 -0600

    feat(sesión 8): añade cuerpo y algunos apuntes para listas

--- Sesiones/Sesion_8.py
+++ Sesiones/Sesion_8.py
@@ -1,9 +1,16 @@
 # -----------------------------------------------
 # Apuntes
 #---------
-# 
+# Listas
+numeros = [1,2,3,4]
+print("La lista {} es de la clase {}".format(numeros, type(numeros)))
+lista = [2, 31.43, True, "soy cadena", 'c', ["otra lista", False, range(2,1)]]
+print(f"Las listas pueden contener distintos tipos de datos\nComo la lista: {lista}")
+# Actualiza valores de la lista
 
-#
+# Esto tiene sentido cuando recordamos los id's de las variables
+
 
 # -----------------------------------------------
 # Ejercicios
+
